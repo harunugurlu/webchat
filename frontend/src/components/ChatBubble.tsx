7b35765f08ee0c0004486f5fc1a9007e28d688a8
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 20:17:54 +0300

    rename message box component

--- frontend/src/components/ChatBubble.tsx
+++ frontend/src/components/ChatBubble.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-export default function MessageBox() {
+export default function ChatBubble() {
     return (
         <div className="p-4 max-w-md relative chat-bubble">
             <div className="p-3 rounded-xl bg-blue-400 text-white">
                 Hello, how can I help you?
             </div>
         </div>
-
-
     )
 }
--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import MessageBox from './MessageBox'
+import ChatBubble from './ChatBubble'
 
 export default function ChatContainer() {
   return (
     <div className="flex flex-col justify-end p-10 border-solid border-2 rounded-lg h-3/6 w-3/6 bg-white">
-      <MessageBox></MessageBox>
+      <ChatBubble></ChatBubble>
     </div>
   )
 }
