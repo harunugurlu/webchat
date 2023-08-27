d12fb8bbe8ba7ad418e295e7e467cf1ca068e3a0
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 20:17:01 +0300

    add chat bubble to the chat container

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MessageBox from './MessageBox'
 
 export default function ChatContainer() {
   return (
-    <div className="flex border-solid border-2 rounded-lg h-3/6 w-3/6 bg-white">
-
+    <div className="flex flex-col justify-end p-10 border-solid border-2 rounded-lg h-3/6 w-3/6 bg-white">
+      <MessageBox></MessageBox>
     </div>
   )
 }
