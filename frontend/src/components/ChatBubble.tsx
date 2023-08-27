063e8f671564c28af265a48b23e20c9127db99df
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 21:29:52 +0300

    add incoming and outgoing chat bubble variants and shadow

--- frontend/src/components/ChatBubble.tsx
+++ frontend/src/components/ChatBubble.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function ChatBubble() {
+interface ChatBubbleProps {
+    messageType?: "incoming" | "outgoing"
+}
+
+export default function ChatBubble(props: ChatBubbleProps) {
+
+    const [messageType, setMessageType] = React.useState<"incoming" | "outgoing">('incoming')
+
     return (
-        <div className="p-4 max-w-md h-fit relative chat-bubble">
-            <div className="p-3 rounded-xl bg-blue-400 text-white">
+        <div className={`p-4 max-w-sm  h-fit relative chat-bubble-${messageType}`}>
+            <div className="p-3 rounded-xl shadow-sm shadow-black bg-blue-400 text-white">
                 Hello, how can I help you?
             </div>
         </div>
