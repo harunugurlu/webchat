7b8c97295b4d630c3df5fcad922ed80ee53ecc36
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 20:16:51 +0300

    add simple chat chat bubble

---
+++ frontend/src/components/MessageBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function MessageBox() {
+    return (
+        <div className="p-4 max-w-md relative chat-bubble">
+            <div className="p-3 rounded-xl bg-blue-400 text-white">
+                Hello, how can I help you?
+            </div>
+        </div>
+
+
+    )
+}
