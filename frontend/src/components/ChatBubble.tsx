5c368e5f60ff48ca97f3af8f644e3796094cbbfd
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 20:19:31 +0300

    add height fit content to chat bubble

--- frontend/src/components/ChatBubble.tsx
+++ frontend/src/components/ChatBubble.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ChatBubble() {
     return (
-        <div className="p-4 max-w-md relative chat-bubble">
+        <div className="p-4 max-w-md h-fit relative chat-bubble">
             <div className="p-3 rounded-xl bg-blue-400 text-white">
                 Hello, how can I help you?
             </div>
