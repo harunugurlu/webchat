87d3055d461482ad5833826beb4bdb2483dd884e
harunugurlu <harunugurlu1@gmail.com>
2023-08-30 17:25:05 +0300

    add send icon

--- frontend/src/components/TextArea.tsx
+++ frontend/src/components/TextArea.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { VscSend } from 'react-icons/vsc'
 
 export default function TextArea() {
     return (
-        <form>
-            <div className="flex flex-col border-t-2 border-slate-500 shadow">
-                <input type="text" className="w-full h-20" />
-                <button className="w-32 m-2 self-end rounded-md bg-teal-500 hover:bg-teal-600">Send</button>
+        <form >
+            <div className="flex flex-col border-2 border-slate-500 shadow p-4 relative">
+                <textarea placeholder='Send your message' className="w-full flex-grow-2 h-20 focus:outline-none resize-none" />
+                <button className="absolute right-0.5 bottom-0 m-2 self-end rounded-md hover:bg-teal-600">
+                    <VscSend size={30}/>
+                </button>
             </div>
         </form>
     )
