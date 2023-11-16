bf77e7645ac3e89efdde0287be27e9b574983e9b
harunugurlu <harunugurlu1@gmail.com>
2023-11-16 23:28:31 +0300

    connect socket client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { socket } from './socket'
 import ChatContainer from './components/ChatContainer';
 import Navbar from './components/Navbar';
 
+
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  
+  useEffect(() => {
+    socket.connect();
+  }, []);
+  
   return (
     <div className="flex flex-col h-screen items-center flex-grow">
       <Navbar />
