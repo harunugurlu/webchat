afb4bee29f5ff865626a2d240606ac9640c4f00b
harunugurlu <harunugurlu1@gmail.com>
2023-11-17 01:03:06 +0300

    chat bubble logic

--- frontend/src/components/atoms/ChatBubble.tsx
+++ frontend/src/components/atoms/ChatBubble.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 interface ChatBubbleProps {
-    messageTypeProp?: "incoming" | "outgoing"
+    message: string;
+    isOutgoing?: boolean; // Added prop to determine message type
 }
 
-export const ChatBubble: React.FC<ChatBubbleProps> = ({ messageTypeProp = "incoming" }) => {
-    const [messageType, setMessageType] = React.useState<"incoming" | "outgoing">("incoming")
-
-    React.useEffect(() => {
-        setMessageType(messageTypeProp)
-    }, [])
+export const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isOutgoing }) => {
+    // Use the isOutgoing prop to set the message type
+    isOutgoing = false;
+    const messageType = isOutgoing ? 'outgoing' : 'incoming';
 
     return (
-        <div className={`p-4 max-w-sm  h-fit relative chat-bubble-${messageType}`}>
-            <div className="p-3 rounded-xl shadow-sm shadow-black bg-blue-400 text-white">
-                Hello, how can I help you?
+        <div className={`p-4 max-w-sm h-fit relative chat-bubble-${messageType}`}>
+            <div className={`p-3 rounded-xl shadow-sm shadow-black ${isOutgoing ? 'bg-green-400' : 'bg-blue-400'} text-white`}>
+                {message}
             </div>
         </div>
-    )
-}
+    );
+};
